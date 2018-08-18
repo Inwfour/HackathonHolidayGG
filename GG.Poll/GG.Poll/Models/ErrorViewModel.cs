a97ea0308b35e14da8a4a5d32acdafce1317d189
Somkiet <somsor@perfenterprise.com>
2018-08-18 11:18:33 +0700

    create project

---
+++ GG.Poll/GG.Poll/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GG.Poll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
